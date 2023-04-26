f2d2f87b8702497f470268e897a1e1b8df70073b
Jude Sajith <judesajith.aj@gmail.com>
2023-04-26 17:36:19 +0530

    sssss

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
 import streamlit as st
+import hydralit_components as hc
 
+#can apply customisation to almost all the properties of the card, including the progress bar
+theme_bad = {'bgcolor': '#FFF0F0','title_color': 'red','content_color': 'red','icon_color': 'red', 'icon': 'fa fa-times-circle'}
+theme_neutral = {'bgcolor': '#f9f9f9','title_color': 'orange','content_color': 'orange','icon_color': 'orange', 'icon': 'fa fa-question-circle'}
+theme_good = {'bgcolor': '#EFF8F7','title_color': 'green','content_color': 'green','icon_color': 'green', 'icon': 'fa fa-check-circle'}
 
+cc = st.columns(4)
 
-import streamlit as st
+with cc[0]:
+ # can just use 'good', 'bad', 'neutral' sentiment to auto color the card
+ hc.info_card(title='Some heading GOOD', content='All good!', sentiment='good',bar_value=77)
 
+with cc[1]:
+ hc.info_card(title='Some BAD BAD', content='This is really bad',bar_value=12,theme_override=theme_bad)
 
-tab1_image = st.image("https://static.streamlit.io/examples/cat.jpg", width=50)
-tab2_image = st.image("https://static.streamlit.io/examples/dog.jpg", width=50)
-tab3_image = st.image("https://static.streamlit.io/examples/owl.jpg", width=50)
+with cc[2]:
+ hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral',bar_value=55)
 
-
-
-tabs = st.container()
-
-with tabs:
-    tab1 = st.tab(label=tab1_image)
-    tab2 = st.tab(label=tab2_image)
-    tab3 = st.tab(label=tab3_image)
-
-
-
-with tab1:
-
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-
-
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-
-
-with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+with cc[3]:
+ #customise the the theming for a neutral content
+ hc.info_card(title='Some NEURAL',content='Maybe...',key='sec',bar_value=5,theme_override=theme_neutral)
