6a42fe7c87e8be1de435720bd55e7e7894d91302
Jude Sajith <judesajith.aj@gmail.com>
2023-04-26 17:40:20 +0530

    ssss

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
 import streamlit as st
 import hydralit_components as hc
-import datetime
 
-#make it look nice from the start
-st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
-
-# specify the primary menu definition
-menu_data = [
-    {'icon': "far fa-copy", 'label':"Left End"},
-    {'id':'Copy','icon':"🐙",'label':"Copy"},
-    {'icon': "fa-solid fa-radar",'label':"Dropdown1", 'submenu':[{'id':' subid11','icon': "fa fa-paperclip", 'label':"Sub-item 1"},{'id':'subid12','icon': "💀", 'label':"Sub-item 2"},{'id':'subid13','icon': "fa fa-database", 'label':"Sub-item 3"}]},
-    {'icon': "far fa-chart-bar", 'label':"Chart"},#no tooltip message
-    {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
-    {'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"}, #can add a tooltip message
-    {'icon': "far fa-copy", 'label':"Right End"},
-    {'icon': "fa-solid fa-radar",'label':"Dropdown2", 'submenu':[{'label':"Sub-item 1", 'icon': "fa fa-meh"},{'label':"Sub-item 2"},{'icon':'🙉','label':"Sub-item 3",}]},
+# define what option labels and icons to display
+option_data = [
+   {'icon': "bi bi-hand-thumbs-up", 'label':"Agree"},
+   {'icon':"fa fa-question-circle",'label':"Unsure"},
+   {'icon': "bi bi-hand-thumbs-down", 'label':"Disagree"},
 ]
 
-over_theme = {'txc_inactive': '#FFFFFF'}
-menu_id = hc.nav_bar(
-    menu_definition=menu_data,
-    override_theme=over_theme,
-    home_name='Home',
-    login_name='Logout',
-    hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
-    sticky_nav=True, #at the top or not
-    sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
-)
+# override the theme, else it will use the Streamlit applied theme
+over_theme = {'txc_inactive': 'white','menu_background':'purple','txc_active':'yellow','option_active':'blue'}
+font_fmt = {'font-class':'h2','font-size':'150%'}
 
-if st.button('click me'):
-  st.info('You clicked at: {}'.format(datetime.datetime.now()))
+# display a horizontal version of the option bar
+op = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
 
-
-if st.sidebar.button('click me too'):
-  st.info('You clicked at: {}'.format(datetime.datetime.now()))
-
-#get the id of the menu item clicked
-st.info(f"{menu_id}")
+# display a version version of the option bar
+op2 = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=False)
