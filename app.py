28dc9eb320c1b909b790403601a3e50578fcb3a0
Jude Sajith <judesajith.aj@gmail.com>
2023-04-26 19:11:54 +0530

    ssss

--- app.py
+++ app.py
@@ -12,10 +12,15 @@ option_data = [
 over_theme = {'txc_inactive': 'white','menu_background':'purple','txc_active':'yellow','option_active':'blue'}
 font_fmt = {'font-class':'h2','font-size':'150%'}
 
+# set the current state of the option bar
+current_state = 0
+
 # display a version version of the option bar
-op2 = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=False)
+op = hc.option_bar(option_definition=option_data[current_state:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
 
 # check if the "Next" button has been clicked
 if st.button("Next"):
-    # display a horizontal version of the option bar
-    op = hc.option_bar(option_definition=option_data[1:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
+    # update the current state of the option bar
+    current_state += 1
+    # display a horizontal version of the option bar with the updated state
+    op = hc.option_bar(option_definition=option_data[current_state:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
