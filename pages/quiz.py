10a2f18862887313c0155cc8a1e1076bea405fba
Jude Sajith <judesajith.aj@gmail.com>
2023-04-26 20:26:00 +0530

    ssss

--- pages/quiz.py
+++ pages/quiz.py
@@ -33,7 +33,7 @@ if 'current_question' not in st.session_state:
 
 
 
-if st.session_state['current_question'] < len(questions):
+if st.session_state['current_question'] =< len(questions):
     current_question = questions[st.session_state['current_question']]
     message(current_question['question'], is_user=False, key=str(
         st.session_state['current_question']))
