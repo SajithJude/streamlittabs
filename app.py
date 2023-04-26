c4fcaf42111b84f92c85ad27b7a0301d2b5154c3
Jude Sajith <judesajith.aj@gmail.com>
2023-04-26 19:10:03 +0530

    ssss

--- app.py
+++ app.py
@@ -3,45 +3,19 @@ import hydralit_components as hc
 
 # define what option labels and icons to display
 option_data = [
-   { 'label':"1"},
-   {'label':"2"},
-   { 'label':"3"},
-   { 'label':"4"},
-   {'label':"5"},
-   { 'label':"6"},
-   { 'label':"7"}
-
+   {'icon': "bi bi-hand-thumbs-up", 'label':"Agree"},
+   {'icon':"fa fa-question-circle",'label':"Unsure"},
+   {'icon': "bi bi-hand-thumbs-down", 'label':"Disagree"},
 ]
 
 # override the theme, else it will use the Streamlit applied theme
-over_theme = {'txc_inactive': 'white','menu_background':'#2953b3','txc_active':'#2953b3','option_active':'white'}
-# font_fmt = {'font-class':'h2','font-size':'150%'}
+over_theme = {'txc_inactive': 'white','menu_background':'purple','txc_active':'yellow','option_active':'blue'}
+font_fmt = {'font-class':'h2','font-size':'150%'}
 
-# display a horizontal version of the option bar
-# op = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,horizontal_orientation=True)
-op = hc.option_bar(option_data, first_select=current_option_index,override_theme=over_theme,horizontal_orientation=True)
+# display a version version of the option bar
+op2 = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=False)
 
-
-current_option_index = st.session_state.get("current_option_index", 0)
-
+# check if the "Next" button has been clicked
 if st.button("Next"):
-    current_option_index = (current_option_index + 1) % len(option_data)
-    st.session_state.current_option_index = current_option_index
-    op = hc.option_bar(option_data, first_select=current_option_index,override_theme=over_theme,horizontal_orientation=True)
-
-
-
-if op == "1":
-    st.write("hello 1")
-elif op == "2":
-    st.write("hello 2")
-elif op == "3":
-    st.write("hello 3")
-elif op == "4":
-    st.write("hello 4")
-elif op == "5":
-    st.write("hello 5")
-elif op == "6":
-    st.write("hello 6")
-elif op == "7":
-    st.write("hello 7")
+    # display a horizontal version of the option bar
+    op = hc.option_bar(option_definition=option_data[1:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
