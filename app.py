2f769113e1003345d068d3afc5658d1a2fae0c68
Jude Sajith <judesajith.aj@gmail.com>
2023-04-26 19:12:49 +0530

    ssss

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ option_data = [
 
 # override the theme, else it will use the Streamlit applied theme
 over_theme = {'txc_inactive': 'white','menu_background':'purple','txc_active':'yellow','option_active':'blue'}
-font_fmt = {'font-class':'h2','font-size':'150%'}
+# font_fmt = {'font-class':'h2','font-size':'150%'}
 
 # set the current state of the option bar
 current_state = 0
 
 # display a version version of the option bar
-op = hc.option_bar(option_definition=option_data[current_state:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
+op = hc.option_bar(option_definition=option_data[current_state:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,horizontal_orientation=True)
 
 # check if the "Next" button has been clicked
 if st.button("Next"):
     # update the current state of the option bar
     current_state += 1
     # display a horizontal version of the option bar with the updated state
-    op = hc.option_bar(option_definition=option_data[current_state:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
+    op = hc.option_bar(option_definition=option_data[current_state:],title='Feedback Response',key='PrimaryOption',override_theme=over_theme,horizontal_orientation=True)
